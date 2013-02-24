4ce2826143ab004cd16fbd765fcc3453713b3763
mtwentytwo22 <mtwentytwo22@gmail.com>
2013-02-24 17:51:25 +0100

    FirstPush

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'a94bfded4d1e43cc5800e69696e5fef492cbb74c2efdbd215526cb39c834e765f40402089ec346319e65143b4fbbf212cf13b6411bab50b32adef6d0ba73459a'
